02db0640739d066555c9fa5e464af88237445e02
Faraaz Khan <faraazkhan212@gmail.com>
2021-04-27 18:36:46 -0400

    Created DEPOSIT command validator class, wrote tests, and made sure they all pass

--- src/main/java/banking/DepositCommandValidator.java
+++ src/main/java/banking/DepositCommandValidator.java
@@ -7,6 +7,69 @@ class DepositCommandValidator {
     }
 
     boolean validate(String[] commandArguments) {
+        if (validateThatAccountExistsInBank(commandArguments)) {
+            if (isDepositingIntoCheckingAccount(commandArguments)) {
+                return depositAmountIntoCheckingAccountIsValid(commandArguments);
+            } else if (isDepositingIntoSavingsAccount(commandArguments)) {
+                return depositAmountIntoSavingsAccountIsValid(commandArguments);
+            } else if (isDepositingIntoCDAccount(commandArguments)) {
+                return false;
+            }
+        }
         return false;
     }
+
+    private boolean validateThatAccountExistsInBank(String[] commandArguments) {
+        try {
+            return bank.getAccounts().containsKey(commandArguments[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean isDepositingIntoCheckingAccount(String[] commandArguments) {
+        try {
+            Account account  = bank.getAccounts().get(commandArguments[1]);
+            return account instanceof CheckingAccount;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean isDepositingIntoSavingsAccount(String[] commandArguments) {
+        try {
+            Account account  = bank.getAccounts().get(commandArguments[1]);
+            return account instanceof SavingsAccount;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean isDepositingIntoCDAccount(String[] commandArguments) {
+        try {
+            Account account  = bank.getAccounts().get(commandArguments[1]);
+            return account instanceof CDAccount;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean depositAmountIntoCheckingAccountIsValid(String[] commandArguments) {
+        double depositAmount = isDouble(commandArguments[2]);
+        return (depositAmount != -1 && depositAmount >= 0 && depositAmount <= 1000);
+    }
+
+    private boolean depositAmountIntoSavingsAccountIsValid(String[] commandArguments) {
+        double depositAmount = isDouble(commandArguments[2]);
+        return (depositAmount != -1 && depositAmount >= 0 && depositAmount <= 2500);
+    }
+
+    private double isDouble(String str) {
+        try {
+            return Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
 }
---
+++ src/test/java/banking/DepositCommandValidatorTest.java
@@ -0,0 +1,188 @@
+package banking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+ class DepositCommandValidatorTest {
+
+    private CommandValidator commandValidator;
+    private Bank bank;
+    private CommandInput commandInput;
+
+    @BeforeEach
+    void setUp() {
+        bank = new Bank();
+        commandValidator = new CommandValidator(bank);
+        commandInput = new CommandInput();
+    }
+
+    @Test
+    void invalid_empty_command() {
+        String[] commandArgs = commandInput.commandToArray("");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void missing_account_id_and_deposit_amount() {
+        String[] commandArgs = commandInput.commandToArray("deposit");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void missing_deposit_amount() {
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void given_account_id_does_not_exist_in_bank() {
+        String[] commandArgs = commandInput.commandToArray("deposit 98765432 100");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void account_id_less_than_eight_digits_is_invalid() {
+        bank.openCheckingAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("deposit 1234567 100");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void account_id_greater_than_eight_digits_is_invalid() {
+        bank.openCheckingAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("deposit 123456789 100");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void account_id_is_not_a_number_is_invalid() {
+        bank.openCheckingAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("deposit 123abcd8 2000");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void cannot_deposit_into_a_cd_account() {
+        bank.openCDAccount("12345678", 0.1, 1000);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 100");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void able_to_deposit_zero_dollars_into_savings() {
+        bank.openSavingsAccount("12345678", 500);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 0");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void able_to_deposit_one_dollar_into_savings() {
+        bank.openSavingsAccount("12345678", 0.07);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void cannot_deposit_negative_dollars_into_savings() {
+        bank.openSavingsAccount("12345678", 0.78);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 -1");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void able_to_deposit_twenty_five_hundred_dollars_into_savings_account() {
+        bank.openSavingsAccount("12345678", 0.93);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 2500");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void cannot_deposit_more_than_twenty_five_hundred_dollars_into_savings() {
+        bank.openSavingsAccount("12345678", 0.63);
+        String[] commandArgs = commandInput.commandToArray("deposit 87654321 2501");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void deposit_zero_dollars_into_checking_account_is_valid() {
+        bank.openCheckingAccount("12345678", 0.57);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 0");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void deposit_one_dollar_into_checking_account_is_valid() {
+        bank.openCheckingAccount("12345678", 0.89);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void cannot_deposit_negative_dollars_into_checking_account() {
+        bank.openCheckingAccount("12345678", 0.29);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 -1");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void able_to_deposit_one_thousand_dollars_into_checking_account() {
+        bank.openCheckingAccount("12345678", 0.21);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1000");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertTrue(isValidCommand);
+    }
+
+    @Test
+    void cannot_deposit_more_than_thousand_dollars_into_checking_account() {
+        bank.openCheckingAccount("12345678", 0.91);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1001");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void deposit_amount_has_unexpected_symbol() {
+        bank.openSavingsAccount("12345678", 0.01);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1,000");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void invalid_deposit_amount_is_not_a_double() {
+        bank.openSavingsAccount("12345678", 0.01);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 h");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+    @Test
+    void invalid_deposit_amount_contains_letters() {
+        bank.openSavingsAccount("12345678", 0.01);
+        String[] commandArgs = commandInput.commandToArray("deposit 12345678 1ab");
+        boolean isValidCommand = commandValidator.validate(commandArgs);
+        assertFalse(isValidCommand);
+    }
+
+
+
+}
