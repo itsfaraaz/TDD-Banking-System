ab2135ff14848bf126f00a4c2d6c72d065ac5640
Faraaz Khan <faraazkhan212@gmail.com>
2021-04-27 18:35:52 -0400

    Finished writing tests for the CREATE command, fixed any failing tests, and completed the entire class

--- src/main/java/banking/CreateCommandValidator.java
+++ src/main/java/banking/CreateCommandValidator.java
@@ -7,6 +7,113 @@ class CreateCommandValidator {
     }
 
     boolean validate(String[] commandArguments) {
+        if (isCreatingCheckingAccount(commandArguments) || isCreatingSavingsAccount(commandArguments)) {
+            return validateCheckingOrSavingsAccountArguments(commandArguments);
+        } else if (isCreatingCDAccount(commandArguments)) {
+            return validateCDAccountArguments(commandArguments);
+        }
         return false;
     }
+
+    private boolean validateCDAccountArguments(String[] commandArguments) {
+        try {
+            return creatingCDAccountHasProperNumberOfArguments(commandArguments) &&
+                    isAValidAccountID(commandArguments[2]) &&
+                    validateThatAccountIDDoesNotExist(commandArguments[2]) &&
+                    isValidAPR(commandArguments[3]) &&
+                    isWithinCDStartingBalanceLimits(commandArguments[4]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean validateThatAccountIDDoesNotExist(String accountID) {
+        return !bank.getAccounts().keySet().contains(accountID);
+    }
+
+    private boolean validateCheckingOrSavingsAccountArguments(String[] commandArguments) {
+        try {
+            return creatingCheckingOrSavingsHasProperNumberOfArguments(commandArguments) &&
+                    validateThatAccountIDDoesNotExist(commandArguments[2]) &&
+                    startsWithBalanceOfZero(commandArguments) &&
+                    isAValidAccountID(commandArguments[2]) &&
+                    isValidAPR(commandArguments[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean creatingCDAccountHasProperNumberOfArguments(String[] commandArguments) {
+        return commandArguments.length == 5;
+    }
+
+    private boolean creatingCheckingOrSavingsHasProperNumberOfArguments(String[] commandArguments) {
+        return commandArguments.length == 4;
+    }
+
+    private boolean isAValidAccountID(String accountID) {
+        if (isInteger(accountID)) {
+            return accountID.length() == 8;
+        }
+        return false;
+    }
+
+    private boolean isValidAPR(String APR) {
+        double parsedAPR;
+        try {
+            parsedAPR = Double.parseDouble(APR);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return(parsedAPR >= 0 && parsedAPR <= 10);
+    }
+
+    private boolean isCreatingCheckingAccount(String[] commandArguments) {
+        try {
+            return commandArguments[1].equalsIgnoreCase("checking");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean isCreatingSavingsAccount(String[] commandArguments) {
+        try {
+            return commandArguments[1].equalsIgnoreCase("savings");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean isCreatingCDAccount(String[] commandArguments) {
+        try {
+            return commandArguments[1].equalsIgnoreCase("cd");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    private boolean startsWithBalanceOfZero(String[] commandArguments) {
+        return commandArguments.length != 5;
+    }
+
+    private boolean isWithinCDStartingBalanceLimits(String startingBalacnceAsString) {
+        double startingBalance;
+        try {
+            startingBalance = Double.parseDouble(startingBalacnceAsString);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return (startingBalance >= 1000 && startingBalance <= 10000);
+    }
+
+    private boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
 }
--- src/test/java/banking/CreateCommandValidatorTest.java
+++ src/test/java/banking/CreateCommandValidatorTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CreateCommandValidatorTest {
-    CommandValidator commandValidator;
+class CreateCommandValidatorTest {
+    private CommandValidator commandValidator;
     private Bank bank;
     private CommandInput commandInput;
 
@@ -18,240 +19,240 @@ public class CreateCommandValidatorTest {
     }
 
     @Test
-    public void empty_input_command_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void invalid_empty_command() {
+        String[] commandArgs = commandInput.commandToArray("");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void missing_account_type_id_and_apr_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void invalid_command_missing_account_type_id_and_apr() {
+        String[] commandArgs = commandInput.commandToArray("create");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void missing_id_and_apr_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void invalid_command_missing_id_and_apr() {
+        String[] commandArgs = commandInput.commandToArray("create checking");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void missing_apr_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void invalid_command_missing_apr() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void missing_amount_for_cd_account_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 12345678 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void invalid_command_missing_starting_balance_for_cd_account() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void account_type_is_all_caps_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create CHECKING 12345678 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void account_type_is_all_caps_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create CHECKING 12345678 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void id_is_less_than_eight_digits_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 1234567 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void id_is_less_than_eight_digits_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 1234567 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void id_is_greater_than_eight_digits_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 123456789 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void id_is_greater_than_eight_digits_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 123456789 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void id_is_not_an_eight_digit_number_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 123abc789 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void id_is_not_an_eight_digit_number_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 123abc789 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void duplicate_checking_id_is_invalid() {
+    void duplicate_checking_id_is_invalid() {
         bank.openCheckingAccount("12345678", 0.1);
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void new_checking_id_is_valid() {
-        bank.addCheckingAccount("12345678", 0.1);
-        String[] commandArr = commandInput.commandToArray("create checking 23456789 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void new_checking_id_is_valid() {
+        bank.openCheckingAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("create checking 13345678 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void duplicate_savings_id_is_invalid() {
-        bank.addSavingsAccount("87654321", 0.1);
-        String[] commandArr = commandInput.commandToArray("create savings 87654321 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void duplicate_savings_id_is_invalid() {
+        bank.openSavingsAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("create savings 12345678 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void new_savings_id_is_valid() {
-        bank.addSavingsAccount("87654321", 0.1);
-        String[] commandArr = commandInput.commandToArray("create savings 98765432 0.1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void new_savings_id_is_valid() {
+        bank.openSavingsAccount("12345678", 0.1);
+        String[] commandArgs = commandInput.commandToArray("create savings 12345638 0.1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void duplicate_cd_id_is_invalid() {
-        bank.addCdAccount("13579135", 0.1, 2000);
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 0.1 2000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void duplicate_cd_id_is_invalid() {
+        bank.openCDAccount("12345678", 0.01, 1000);
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.1 1000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void new_cd_id_is_valid() {
-        bank.addCdAccount("13579135", 0.1, 2000);
-        String[] commandArr = commandInput.commandToArray("create cd 53197531 0.1 2000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void new_cd_id_is_valid() {
+        bank.openCDAccount("12345678", 0.01, 1000);
+        String[] commandArgs = commandInput.commandToArray("create cd 12345378 0.1 1000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void apr_is_zero_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 0");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void apr_is_zero_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 0");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void apr_is_one_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void apr_is_one_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void apr_is_ten_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 10");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void apr_is_ten_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 10");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void apr_is_eleven_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 11");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void apr_is_eleven_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 11");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void apr_is_negative_value_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 -1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void apr_is_negative_value_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 -1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void apr_is_one_thousand_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 1000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void apr_is_one_thousand_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 1000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void apr_is_not_a_double_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create checking 12345678 0.a");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void apr_is_not_a_double_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create checking 12345678 0.a");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_zero_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 0");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_is_zero_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.99 0");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_one_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_is_one_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 1 1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_999_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 999");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_is_999_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.39 999");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_one_thousand_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 1000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void amount_for_cd_is_one_thousand_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.23 1000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_ten_thousand_is_valid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 10000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertTrue(actual);
+    void amount_for_cd_is_ten_thousand_is_valid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.99 10000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertTrue(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_10001_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 10001");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_is_10001_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 1.04 10001");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_is_a_negative_value_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 -1");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_is_a_negative_value_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.83 -1");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
     void amount_for_cd_is_hundred_thousand_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 100000");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.87 100000");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void amount_for_cd_has_letters_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1 9bf");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void amount_for_cd_has_letters_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create cd 12345678 0.21 6b");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
     @Test
-    public void account_type_is_misspelled_is_invalid() {
-        String[] commandArr = commandInput.commandToArray("create save 87654321 0.21");
-        boolean actual = commandValidator.validate(commandArr);
-        assertFalse(actual);
+    void account_type_is_misspelled_is_invalid() {
+        String[] commandArgs = commandInput.commandToArray("create save 12345678 0.01");
+        boolean isCommandValid = commandValidator.validate(commandArgs);
+        assertFalse(isCommandValid);
     }
 
-    @Test
-    public void command_is_trying_to_create_cd() {
-        String[] commandArr = commandInput.commandToArray("create cd 13579135 1.00 2000");
-        commandValidator validator = new commandValidator(bank);
-        boolean actual = validator.validationIfCd(commandArr);
-        assertTrue(actual);
-    }
+//    @Test
+//    void command_is_trying_to_create_cd() {
+//        String[] commandArgs = commandInput.commandToArray("create cd 13579135 1.00 2000");
+//        CommandValidator validator = new CommandValidator(bank);
+//        boolean isCommandValid = validator.validationIfCd(commandArgs);
+//        assertTrue(isCommandValid);
+//    }
 }
