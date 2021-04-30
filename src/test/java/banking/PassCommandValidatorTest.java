c2613b67f8ed13cc0f7f35970358f7e4fa75b9d8
Faraaz Khan <faraazkhan212@gmail.com>
2021-04-30 16:49:40 -0400

    Changed class name

--- src/test/java/banking/PassCommandValidatorTest.java
+++ src/test/java/banking/PassCommandValidatorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class PassCommandValidationTest {
+class PassCommandValidatorTest {
 
     private CommandValidator commandValidator;
     private CommandInput commandInput;
